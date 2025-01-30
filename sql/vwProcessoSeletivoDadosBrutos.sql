e12610c913eab5086ae2d65b9aeaec7508b25c57
Edson Miranda <112988565+EdsonGMiranda@users.noreply.github.com>
2025-01-30 17:45:06 -0300

    Delete sql/vwProcessoSeletivoDadosBrutos.sql

--- sql/vwProcessoSeletivoDadosBrutos.sql
+++
@@ -1,4 +0,0 @@
-SELECT  [NmEmail],[CdEscola],[NmEscola], [NmProcessoSeletivo],[DtInscricao],[DtCadastro], [NuInscricao], [CdConcurso],[CdCandidato]
-  FROM [BI_PROCESSO_SELETIVO].[dbo].[vwProcessoSeletivoDadosBrutos]
-  WHERE [NuAnoIngresso] > 2013;
-
